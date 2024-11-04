74e7a70a228f2fbccac1c5ce239518c59f325ecc
SMB-TEC2-14 <L24550835@chihuahua2.tecnm.mx>
2024-11-04 17:40:42 -0600

    Add files via upload

---
+++ EVA3_1_METODOS/src/eva3_1_metodos/EVA3_1_METODOS.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_1_metodos;
+
+/**
+ *
+ * @author santy
+ */
+public class EVA3_1_METODOS {
+    public static void main(String[] args) {
+    //El uso de los metodos tiene como nombre "Invocación" o "Llamada"
+        imprimirMensaje("Hi world :D !!");
+    }
+    
+    //Header (Encabezado)
+    //El objetivo es Imprimir  un mensaje
+    //Recomendación: Se inicia en minisculas y las siguentes palabras inician en mayusculas, verbo
+    public static void imprimirMensaje(String mensaje){//Esto es una declaración
+        System.out.println("Tu Mensaje: " + mensaje);
+        
+    }
+}
